29b7f023b75edd1efa2d08dada40a60bcbf0c31c
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-10-22 12:11:58 +0900

    課題18: studentMapの作成と更新機能を追加(feat: add studentMap with update functionality)

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,23 +1,68 @@
 package raisetech.StudentManagement;
 
 import ch.qos.logback.core.util.StringUtil;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  private String name = "Daiki Tanaka";
+  private String age = "31";
+
+  private Map<String, String>studentMap = new HashMap<>();
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
+  @PostMapping("/studentMap")
+  public void addStudentMap(String nameMap, String ageMap){
+    studentMap.put(nameMap, ageMap);
+  }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello, World!";
+  @GetMapping("/studentMap")
+  public String getStudentMap() {
+    StringBuilder result = new StringBuilder();
+    for (String key : studentMap.keySet()){
+      result.append(key)
+          .append("さんは")
+          .append(studentMap.get(key))
+          .append("歳です。\n");
+    }
+    return result.toString();
+  }
+
+  @PutMapping("/studentMap/{nameMap}")
+  public void updateStudentAge(@PathVariable String nameMap,@RequestParam String ageMap){
+    studentMap.put(nameMap, ageMap);
+  }
+
+
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
+  }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age){
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/studentName")
+  public void updateStudentName(String name){
+    this.name = name;
   }
 
 }
