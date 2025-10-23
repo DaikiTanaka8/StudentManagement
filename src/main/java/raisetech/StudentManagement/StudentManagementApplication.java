36358c1fd7c8fa186ec7acebda7bcd606eb1c57f
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-10-23 14:15:36 +0900

    [課題20]全生徒情報の表示(feat: add getAllStudent endpoint to list all students)

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,10 +1,15 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,15 +20,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagementApplication {
 
-  private String name = "Daiki Tanaka";
-  private String age = "31";
+  @Autowired
+  private StudentRepository repository;
 
   private Map<String, String>studentMap = new ConcurrentHashMap<>();
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
+
+
+  // 下記は講義18の講義内のコード＋講義20で編集
+  @GetMapping("/student")
+  public String getStudent(@RequestParam String name) {
+    Student student = repository.searchByName(name);
+    return student.getName() + " " + student.getAge() + "歳";
+  }
+
+  @PostMapping("/student")
+  public void registerStudent(@RequestParam String name,@RequestParam int age){
+    repository.registerStudent(name ,age);
+  }
+
+  @PatchMapping("/student")
+  public void updateStudentName(String name, int age){
+    repository.updateStudent(name, age);
+  }
+
+  @DeleteMapping("/student")
+  public void deleteStudent(String name){
+    repository.deleteStudent(name);
+  }
+
+  // 下記は講義20の課題のコード
+  @GetMapping("/students")
+  public String getAllStudent() {
+    StringBuilder result = new StringBuilder();
+    List<Student> students = repository.getAllStudent();
+    for (Student student : students){
+      result.append(student.getName())
+          .append("　")
+          .append(student.getAge())
+          .append("歳\n");
+    }
+    return result.toString();
+  }
+
+
+  // 下記は講義18の課題のコード
   @PostMapping("/studentMap")
   public void addStudentMap(@RequestParam String nameMap,@RequestParam String ageMap){
     studentMap.put(nameMap, ageMap);
@@ -45,22 +89,4 @@ public class StudentManagementApplication {
   public void updateStudentAge(@PathVariable String nameMap,@RequestParam String ageMap){
     studentMap.put(nameMap, ageMap);
   }
-
-
-  @GetMapping("/studentInfo")
-  public String getStudentInfo() {
-    return name + " " + age + "歳";
-  }
-
-  @PostMapping("/studentInfo")
-  public void setStudentInfo(@RequestParam String name,@RequestParam String age){
-    this.name = name;
-    this.age = age;
-  }
-
-  @PostMapping("/studentName")
-  public void updateStudentName(@RequestParam String name){
-    this.name = name;
-  }
-
 }
