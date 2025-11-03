8db5f54df6c34ad9323bea7817b734e68432cf4b
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-11-03 21:10:56 +0900

    [課題28] 新規受講生情報を登録する処理の実装

--- src/main/java/raisetech/studentmanagement/repository/StudentRepository.java
+++ src/main/java/raisetech/studentmanagement/repository/StudentRepository.java
@@ -1,10 +1,12 @@
 package raisetech.studentmanagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
+import raisetech.studentmanagement.domain.StudentDetail;
 
 @Mapper
 public interface StudentRepository {
@@ -14,4 +16,9 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourses();
+
+@Insert("INSERT INTO students(student_id,name, furigana, nickname, email, city, age, gender, remark)"
+    + "VALUES(#{studentId}, #{name}, #{furigana}, #{nickname}, #{email}, #{city}, #{age}, #{gender}, #{remark})")
+  void insertStudent(Student student);
+
 }
--- src/main/java/raisetech/studentmanagement/service/StudentService.java
+++ src/main/java/raisetech/studentmanagement/service/StudentService.java
@@ -1,10 +1,12 @@
 package raisetech.studentmanagement.service;
 
 import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
+import raisetech.studentmanagement.domain.StudentDetail;
 import raisetech.studentmanagement.repository.StudentRepository;
 
 @Service
@@ -23,5 +25,14 @@ public class StudentService {
 
   public List<StudentCourse> searchStudentCourseList() {
     return repository.searchStudentCourses();
+  }
+
+  public void registerStudent(StudentDetail studentDetail) {
+
+    // IDを設定
+    studentDetail.getStudent().setStudentId(UUID.randomUUID().toString());
+
+    // ここで実際の登録処理
+    repository.insertStudent(studentDetail.getStudent());
   }
 }
