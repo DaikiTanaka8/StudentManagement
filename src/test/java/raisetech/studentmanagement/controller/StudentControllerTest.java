f365e5ce1cc21b68b24fe3f24b9f9eed49636a9b
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-11 11:39:36 +0900

    [課題41]講義41の実装。コントローラーのテスト作成。受講生詳細一覧検索のテスト追加。受講生詳細ID検索のテスト追加。受講生登録のテスト追加。

---
+++ src/test/java/raisetech/studentmanagement/controller/StudentControllerTest.java
@@ -0,0 +1,197 @@
+package raisetech.studentmanagement.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import raisetech.studentmanagement.controller.converter.StudentConverter;
+import raisetech.studentmanagement.data.Student;
+import raisetech.studentmanagement.data.StudentCourse;
+import raisetech.studentmanagement.domain.StudentDetail;
+import raisetech.studentmanagement.repository.StudentRepository;
+import raisetech.studentmanagement.service.StudentService;
+
+
+@WebMvcTest(StudentController.class) //MEMO: 「StudentControllerだけ起動するWeb層のテストですよ」という宣言。テスト用のSpringBootが立ち上がる。
+class StudentControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc; //MEMO: SpringBootが用意しているMockの仕組み。HTTPリクエストを擬似的に投げるための仕組み。
+
+  @Autowired
+  private ObjectMapper objectMapper; //MEMO: JSONの文字列で変換する(自分で追加)
+
+  @MockBean
+  private StudentService service; //MEMO: Mock化したサービスを用意しておく。コントローラーはMockBean。
+
+  @MockBean
+  private StudentRepository repository; //MEMO: これがないとMyBatisのリポジトリを呼び出してしまう。だから、リポジトリもMock化してしまった。
+
+  @MockBean
+  private StudentConverter converter; //MEMO: これも追加しておく！
+
+
+  private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+  //MEMO: Bean Validation（Jakarta Validation / Hibernate Validator）を使うための“バリデータ本体”を生成するコード。
+  // 「Javaのバリデーションを手動で実行できる Validator インスタンスを取得している」。Controller の @Valid の裏側で動いているやつを、自分で呼び出せるようにしている。
+
+
+  @Test
+  void 受講生詳細の一覧検索が実行できてからのリストが返ってくること() throws Exception {
+    when(service.searchStudentList()).thenReturn(List.of(new StudentDetail()));
+
+    mockMvc.perform(get("/studentList")) //MEMO: テスト用の疑似HTTPクライアント（mockMvc）を使って、GET/studentListを実行。
+        .andExpect(status().isOk()) //MEMO: Controllerからの戻り値（HTTP ステータスコード）が 200 OK であることを確認。
+        .andExpect(content().json("[]"));
+
+    verify(service, times(1)).searchStudentList();
+  }
+
+  @Test
+  void 受講生詳細の受講生で適切な値を入力したときに入力チェックに異常が発生しないこと(){
+    Student student = new Student();
+    student.setStudentId("123");
+    student.setName("テスト太郎");
+    student.setFurigana("てすとたろう");
+    student.setEmail("test@example.com");
+    student.setCity("東京都");
+    student.setAge(100);
+    student.setGender("その他");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+    //MEMO: studentに付けたバリデーションアノテーション（@NotNull, @Pattern, @Size など）が破られていないかチェックして、違反していた項目を全部返す。
+
+    Assertions.assertEquals(0, violations.size()); //MEMO: 「違反は発生しないよ」という期待をテストしている。
+    assertThat(violations.size()).isEqualTo(0); //MEMO: assertThatの方が直感的。
+  }
+
+  @Test
+  void 受講生詳細の受講生でメールアドレスに不正な値を入力したときに入力チェックにかかること(){
+    Student student = new Student();
+    student.setName("テスト太郎");
+    student.setFurigana("てすとたろう");
+    student.setEmail("example.com");
+    student.setCity("東京都");
+    student.setAge(100);
+    student.setGender("その他");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+    //MEMO: studentに付けたバリデーションアノテーション（@NotNull, @Pattern, @Size など）が破られていないかチェックして、違反していた項目を全部返す。
+
+    Assertions.assertEquals(1, violations.size()); //MEMO: 「違反は１つだけ発生する」という期待をテストしている。
+    assertThat(violations.size()).isEqualTo(1); //MEMO:「バリデーションエラーが 1 件だけ発生していること」を確認している。
+    assertThat(violations).extracting("message").containsOnly("正しいメールアドレスを入力してください"); //MEMO: 「どんなエラーメッセージが出ているか」を検証している。
+
+  }
+
+  @Test
+  void 受講生検索のID検索が実行したら受講生詳細が返ってくること() throws Exception {
+    Student student = new Student();
+    student.setStudentId("12345");
+    student.setName("テスト太郎");
+    student.setFurigana("てすとたろう");
+    student.setNickname("テストさん");
+    student.setEmail("test@example.com");
+    student.setCity("東京");
+    student.setAge(100);
+    student.setGender("その他");
+    student.setRemark("");
+
+    StudentDetail studentDetail = new StudentDetail(student,List.of());
+
+    when(service.searchStudentById("12345")).thenReturn(studentDetail);
+
+    mockMvc.perform(get("/student/12345"))
+        .andExpect(status().isOk())
+        .andExpect(content().json("""
+            {
+                "student": {
+                    "studentId": "12345",
+                    "name": "テスト太郎",
+                    "furigana": "てすとたろう",
+                    "nickname": "テストさん",
+                    "email": "test@example.com",
+                    "city": "東京",
+                    "age": 100,
+                    "gender": "その他",
+                    "remark": "",
+                    "deleted": false
+                },
+                "studentCourseList": []
+            }
+            """));
+
+    verify(service, times(1)).searchStudentById("12345");
+
+  }
+
+  @Test
+  void 受講生登録を実行したら登録した受講生詳細が返ってくること() throws Exception {
+    // 事前準備
+    // 送るデータ
+    Student inputStudent = new Student();
+    inputStudent.setStudentId("test-id-123");
+    inputStudent.setName("テスト太郎");
+    inputStudent.setFurigana("てすとたろう");
+    inputStudent.setEmail("test@example.com");
+    inputStudent.setCity("東京都");
+    inputStudent.setAge(100);
+    inputStudent.setGender("その他");
+
+    StudentCourse course =new StudentCourse();
+    course.setCourseName("テストコース");
+
+    StudentDetail inputStudentDetail = new StudentDetail(inputStudent,List.of(course));
+
+    // 返ってくるデータ
+    Student respenseStudent = new Student();
+    respenseStudent.setStudentId("test-id-123"); //MEMO: 自動生成されたID
+    respenseStudent.setName("テスト太郎");
+    respenseStudent.setFurigana("てすとたろう");
+    respenseStudent.setEmail("test@example.com");
+    respenseStudent.setCity("東京都");
+    respenseStudent.setAge(100);
+    respenseStudent.setGender("その他");
+
+    StudentCourse responseCourse = new StudentCourse();
+    responseCourse.setCourseName("テストコース");
+
+    StudentDetail responseStudentDetail = new StudentDetail(respenseStudent,List.of(responseCourse));
+
+    // Mock設定
+    when(service.registerStudent(any(StudentDetail.class))).thenReturn(responseStudentDetail);
+    //MEMO: any()を使うことで、「どんなStudentDetailでもOK」とする。
+
+    // 実行 & 検証
+    mockMvc.perform(post("/registerStudent")
+        .contentType(MediaType.APPLICATION_JSON) //MEMO: Content-Typeを指定。
+        .content(objectMapper.writeValueAsString(inputStudentDetail))) //MEMO: JSONの文字列を指定。StudentDetailをStringにしてくれている。
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).registerStudent(any(StudentDetail.class));
+
+  }
+
+
+
+
+
+}
