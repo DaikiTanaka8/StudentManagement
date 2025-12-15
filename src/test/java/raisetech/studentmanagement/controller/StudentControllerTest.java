f7ff55a0aa4f0ea461f67e3c924299d6ec2dbc07
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-16 07:51:10 +0900

    [課題42]不要インポートの削除

--- src/test/java/raisetech/studentmanagement/controller/StudentControllerTest.java
+++ src/test/java/raisetech/studentmanagement/controller/StudentControllerTest.java
@@ -22,7 +22,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import raisetech.studentmanagement.controller.converter.StudentConverter;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
 import raisetech.studentmanagement.domain.StudentDetail;
