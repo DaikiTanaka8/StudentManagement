19f77d956ed8b1eed410b8e68677fc5998e1d870
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-18 21:57:43 +0900

    [講義43]テスト用のapplication.propertiesの追加。

--- src/test/java/raisetech/studentmanagement/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/studentmanagement/repository/StudentRepositoryTest.java
@@ -2,9 +2,14 @@ package raisetech.studentmanagement.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 
 @MybatisTest
 class StudentRepositoryTest {
 
+  @Test
+  void test(){
+
+  }
 }
