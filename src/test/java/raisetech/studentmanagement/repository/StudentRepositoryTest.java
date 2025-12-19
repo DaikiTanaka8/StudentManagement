20abeb840956da94c878149d3582788b7e16a587
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-19 11:36:11 +0900

    [講義43]受講生の全件検索のテスト追加。

--- src/test/java/raisetech/studentmanagement/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/studentmanagement/repository/StudentRepositoryTest.java
@@ -1,15 +1,23 @@
 package raisetech.studentmanagement.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import raisetech.studentmanagement.data.Student;
 
 @MybatisTest
 class StudentRepositoryTest {
 
-  @Test
-  void test(){
+  @Autowired
+  private StudentRepository sut;
 
+  @Test
+  void 受講生の全件検索が行えること(){
+    List<Student> actyal = sut.searchStudent();
+    assertThat(actyal.size()).isEqualTo(4);
   }
 }
