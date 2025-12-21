c4a86b70c57050f39d2764f67c876f67d7c8ece1
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-21 13:27:53 +0900

    [課題43(修正3)]不要なimportの削除

--- src/test/java/raisetech/studentmanagement/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/studentmanagement/repository/StudentRepositoryTest.java
@@ -8,8 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourse;
 
