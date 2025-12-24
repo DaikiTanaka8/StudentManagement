7ef7daf4def240f45d1d28b6f0ee37125357fd14
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-25 08:24:34 +0900

    [課題44]StudentRepositoryのMEMO削除。

--- src/main/java/raisetech/studentmanagement/repository/StudentRepository.java
+++ src/main/java/raisetech/studentmanagement/repository/StudentRepository.java
@@ -1,5 +1,4 @@
 package raisetech.studentmanagement.repository;
-//MEMO: データベースそのものって思っていればいい。
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,17 +10,17 @@ import raisetech.studentmanagement.data.StudentCourse;
  * 受講生情報を扱うリポジトリ
  * 全件検索や単一条件での検索、コース情報の検索が行えクラスです。
  */
-@Mapper //MEMO: 「MyBatisが管理しないといけない」という指示。SpringBootにMyBatisのMapperであることを伝えている。
+@Mapper
 public interface StudentRepository {
 
   /**
    * 受講生の全件検索を行います。
    * @return 受講生一覧（全件）
    */
-  List<Student> searchStudent(); //MEMO: Listで返します。MyBatisがちゃんとListを認識する。
+  List<Student> searchStudent();
 
   /**
-   * 受講生の検索を行います。 //MEMO: 受講生情報のID検索。IDで単一の受講生を取得してくる。（自分で作れていた！）
+   * 受講生の検索を行います。
    * @return 受講生。ID検索した受講生に関する情報一覧。
    */
   Student searchStudentById(String studentId);
@@ -43,8 +42,7 @@ public interface StudentRepository {
    * 受講生を新規登録します。IDに関しては自動採番を行う（UUID）。
    * @param student 受講生
    */
-  void registerStudent(Student student); //MEMO: INSERTだから登録した後何も返さないのでvoid。
-  //MEMO: IDの管理は自分でしたくない。自動採番にして生成したりUUIDにしたり。MAXに＋１するのはOK。（ちなみに講義内ではAUTO_INCREMENTで自動連番にしている。）
+  void registerStudent(Student student);
 
   /**
    * 受講生コース情報を新規登録します。IDに関しては自動採番を行う（UUID）。
