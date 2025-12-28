97e7ffac12821fd079b18bbe3327b7b5036264c7
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2025-12-28 10:56:45 +0900

    [課題44]StudentCourseStatusのstatusにNotBlankとPatternの追加。

--- src/main/java/raisetech/studentmanagement/data/StudentCourseStatus.java
+++ src/main/java/raisetech/studentmanagement/data/StudentCourseStatus.java
@@ -1,6 +1,8 @@
 package raisetech.studentmanagement.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,5 +13,9 @@ public class StudentCourseStatus {
 
   private String statusId;
   private String courseId;
+
+  @NotBlank(message = "申込状況を入力してください")
+  @Pattern(regexp = "^(仮申込|本申込|受講中|受講終了)$", message = "申込状況は 仮申込・本申込・受講中・受講終了 のいずれかで入力してください")
   private String status;
+
 }
