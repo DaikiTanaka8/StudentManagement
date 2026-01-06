19f372ee7f8b11a06e2ae8a0204f88de9fc2420a
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2026-01-06 09:14:28 +0900

    [課題44]StudentCourseAssembler.javaの作成。

---
+++ src/main/java/raisetech/studentmanagement/domain/assembler/StudentCourseAssembler.java
@@ -0,0 +1,35 @@
+package raisetech.studentmanagement.domain.assembler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+import raisetech.studentmanagement.data.StudentCourse;
+import raisetech.studentmanagement.data.StudentCourseStatus;
+
+@Component
+public class StudentCourseAssembler {
+
+  /**
+   * 全件の受講生コース情報にコース申込状況を紐づけるメソッドです。
+   *
+   * @param studentCourseList 受講生コース情報リスト
+   * @param studentCourseStatusList コース申込状況リスト
+   * @return コース申込状況が紐づいた受講生コースリスト
+   */
+  public List<StudentCourse> assembleCourseListWithStatus(List<StudentCourse> studentCourseList, List<StudentCourseStatus> studentCourseStatusList){
+
+    Map<String, StudentCourseStatus> studentCourseStatusMap = new HashMap<>();
+
+    for(StudentCourseStatus studentCourseStatus : studentCourseStatusList){
+      studentCourseStatusMap.put(studentCourseStatus.getCourseId() , studentCourseStatus);
+    }
+
+    for (StudentCourse studentCourse : studentCourseList){
+      studentCourse.setStatus(studentCourseStatusMap.get(studentCourse.getCourseId()));
+    }
+
+    return studentCourseList;
+  }
+
+}
