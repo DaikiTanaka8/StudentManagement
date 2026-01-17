0be70cd3149bd33d8590574711adb7b315507738
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2026-01-17 15:53:43 +0900

    [課題44]バリデーションエラーメソッド（MethodArgumentNotValidException）の追加。

--- src/main/java/raisetech/studentmanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/raisetech/studentmanagement/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -59,6 +60,17 @@ public class GlobalExceptionHandler {
 
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     //MEMO: .statsでステータス指定、HTTPステータスでノットファウンドリクエスト404番、.bodyで中身を作れる、今回はゲットメッセージでそのままメッセージを表示する。
+  }
+
+  /**
+   * MethodArgumentNotValidException用のハンドラです。
+   * @param ex 実際に発生した例外オブジェクト
+   * @return 400BAD_REQUESTを返す
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+    logger.warn("バリデーションエラーが発生しました: {}", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
   }
 
   /**
