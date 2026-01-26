a02c017bcf6ac8fe37b077c7d03df3153bf5f7e7
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2026-01-26 09:03:02 +0900

    [課題44]StudentCourseAssemblerの仕様コメント（同一のコースIDの場合は最後に検出したものを採用する。ただ、コースとステータスは１：１のためこの挙動で問題ない）の追加。

--- src/main/java/raisetech/studentmanagement/domain/assembler/StudentCourseAssembler.java
+++ src/main/java/raisetech/studentmanagement/domain/assembler/StudentCourseAssembler.java
@@ -12,6 +12,8 @@ public class StudentCourseAssembler {
 
   /**
    * 全件の受講生コース情報にコース申込状況を紐づけるメソッドです。
+   * 注意：同一のcourseIdに対して複数のstatusが存在する場合、最後に処理されたstatusで上書きされます。
+   * 現在の要件では１コース；１ステータスのため、この挙動で問題ありません。
    *
    * @param studentCourseList 受講生コース情報リスト
    * @param studentCourseStatusList コース申込状況リスト
