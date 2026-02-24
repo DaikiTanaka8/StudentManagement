da78c015dd9d7444d0097fee5dcbfe9fdc095808
Daiki Tanaka <d28k8.vpm.ccc@gmail.com>
2026-02-24 10:50:55 +0900

    StudentConverterTestのリファクタリング。MEMOコメント削除。

--- src/test/java/raisetech/studentmanagement/domain/converter/StudentConverterTest.java
+++ src/test/java/raisetech/studentmanagement/domain/converter/StudentConverterTest.java
@@ -21,8 +21,6 @@ class StudentConverterTest {
 
   @Test
   void 受講生詳細の受講生や受講生コース情報の変換を行うコンバーターが正しく動作していること() {
-
-    // 事前準備
     Student student1 = new Student();
     student1.setStudentId("1");
     student1.setName("テスト太郎1");
@@ -44,10 +42,8 @@ class StudentConverterTest {
     List<Student> studentList = List.of(student1, student2);
     List<StudentCourse> studentCourseList = List.of(studentCourse1, studentCourse2, studentCourse3);
 
-    // 実行
     List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
 
-    // 検証
      assertEquals(2, actual.size());
 
      StudentDetail actualStudentDetail1 = actual.get(0);
@@ -62,13 +58,10 @@ class StudentConverterTest {
      assertEquals("テスト太郎2", actualStudentDetail2.getStudent().getName());
      assertEquals(1, actualStudentDetail2.getStudentCourseList().size());
      assertEquals("テストコース3", actualStudentDetail2.getStudentCourseList().get(0).getCourseName());
-
   }
 
-  @Test //MEMO: 解答を模して作成。フィールドではなくインスタンスを比較している。
+  @Test
   void 受講生のリストと受講生コース情報のリストを渡して受講生詳細のリストが作成できること() {
-
-    // 事前準備
     Student student1 = createStudent1();
     Student student2 = createStudent2();
 
@@ -85,19 +78,14 @@ class StudentConverterTest {
     List<Student> studentList = List.of(student1, student2);
     List<StudentCourse> studentCourseList = List.of(studentCourse1, studentCourse2, studentCourse3);
 
-    // 実行
     List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
 
-    // 検証
     assertThat(actual.get(0).getStudent()).isEqualTo(student1);
     assertThat(actual.get(0).getStudentCourseList()).isEqualTo(List.of(studentCourse1,studentCourse2));
-
   }
 
-  @Test //MEMO: 解答より作成。
+  @Test
   void 受講生のリストと受講生コース情報のリストを渡した時に紐づかない受講生コース情報は除外されること() {
-
-    // 事前準備
     Student student1 = createStudent1();
     Student student2 = createStudent2();
 
@@ -108,13 +96,10 @@ class StudentConverterTest {
     List<Student> studentList = List.of(student1, student2);
     List<StudentCourse> studentCourseList = List.of(studentCourse3);
 
-    // 実行
     List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
 
-    // 検証
     assertThat(actual.get(0).getStudent()).isEqualTo(student1);
     assertThat(actual.get(0).getStudentCourseList()).isEmpty();
-
   }
 
 
@@ -147,5 +132,4 @@ class StudentConverterTest {
     student2.setDeleted(false);
     return student2;
   }
-
 }
